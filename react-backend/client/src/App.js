d4c4fb9e7889fc3b489edf7994f4d671cfd93606
grindlock <grindlock@users.noreply.github.com>
2018-01-19 22:18:00 -0500

    Add files via upload

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  state = {users: []}
+  state = {email:'', password:''};
 
-  componentDidMount() {
+  /**componentDidMount() {
     fetch('/users')
       .then(res => res.json())
       .then(users => this.setState({ users }));
+  }**/
+
+  onChange = (e) => {
+    const newState = this.state;
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+  }
+
+  onSubmit = (e) => {
+    var post = {
+      "email":this.state.email,
+      "password":this.state.password
+    }
+    axios.post('http://localhost:3000/signup', post)
+      .then(function (result){
+        console.log(result);
+        if(result.data.code === 200){
+          console.log("HI");
+        }
+
+      })
+      .catch(function (error) {
+     console.log(error);
+   });
   }
 
   render() {
+    const { email, password } = this.state;
     return (
       <div className="App">
         <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
+        <form onSubmit={ this.onSubmit }>
+          <label>Email:</label>
+          <input type="email" name="email" value={email} onChange={this.onChange}/>
+          <label>Paswword:</label>
+          <input type="password" name="password" value={password} onChange={this.onChange}/>
+          <button type="submit">Login/sign Up</button>
+        </form>
       </div>
+
     );
   }
 }
