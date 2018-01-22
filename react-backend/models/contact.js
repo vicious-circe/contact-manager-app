9da6f8ecd84eab6ae32f9132571c56bcde066e0d
Eoin Reilly <reilly_eoin@knights.ucf.edu>
2018-01-22 15:56:37 -0500

    Contact schema

--- react-backend/models/contact.js
+++ react-backend/models/contact.js
@@ -1,23 +1,33 @@
 const mongoose = require('mongoose');
 
 const ContactSchema = new mongoose.Schema({
+    _userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    contact_name: {
+      type: String,
+      required: true,
+      minlength: 1,
+      trim: true
+    },
+
+    phone_number: {
+      type: Number,
+      required: false,
+      minlength: 10,
+      trim: true
+    },
+
     email: {
         type: String,
-        required: true,
+        required: false,
         trim: true,
-        minlength: 1, 
+        minlength: 1,
         unique: true
-    }, 
-    password: {
-        type: String, 
-        required: true, 
-        minlength: 6
-    }, 
-    _userId: {
-        type: mongoose.Schema.Types.ObjectId, 
-        required: true
-    }
-}); 
+    }  
+
+});
 
 var Contact = mongoose.model('Contact', ContactSchema);
 module.exports = {Contact};
