7f0263e68eaeb7a0d5ef3075a67e02e2be48dc6c
grindlock <sergioenriqueperez@gmail.com>
2018-01-23 01:43:57 -0500

    Added bootstrap to the project. Missing validation and hashing.

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import {Form, FormGroup, FormControl, ControlLabel, Col, Button } from 'react-bootstrap';
 
 class App extends Component {
-  state = {email:'', password:''};
+  state = { email:'', password:'' };
 
-  /**componentDidMount() {
+  /** This will come handy when retriving the contact list
+    componentDidMount() {
     fetch('/users')
       .then(res => res.json())
       .then(users => this.setState({ users }));
@@ -18,11 +20,12 @@ class App extends Component {
   }
 
   onSubmit = (e) => {
-    var post = {
+    var data = {
       "email":this.state.email,
       "password":this.state.password
     }
-    axios.post('http://localhost:3000/signup', post)
+    var login = JSON.stringify(data);
+    axios.post('http://localhost:3000/signup', login)
       .then(function (result){
         console.log(result);
         if(result.data.code === 200){
@@ -33,24 +36,49 @@ class App extends Component {
       .catch(function (error) {
      console.log(error);
    });
+   this.setState({name:'', password:''});
   }
 
   render() {
     const { email, password } = this.state;
     return (
       <div className="App">
+        <header className="App-header">
         <h1>Users</h1>
-        <form onSubmit={ this.onSubmit }>
-          <label>Email:</label>
-          <input type="email" name="email" value={email} onChange={this.onChange}/>
-          <label>Paswword:</label>
-          <input type="password" name="password" value={password} onChange={this.onChange}/>
-          <button type="submit">Login/sign Up</button>
-        </form>
+        </header>
+
+        <div className="content-body">
+        <Form horizontal onSubmit={ this.onSubmit }>
+          <FormGroup controlId="formHorizontalEmail" >
+            <Col componentClass={ControlLabel} sm={2} smOffset={2}>Email:</Col>
+            <Col sm={4}>
+              <FormControl type="email" name="email" value={email} placeHolder="hello@myemail.com" onChange={this.onChange}/>
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ ControlLabel } sm={2} smOffset={2}>Paswword:</Col>
+            <Col sm={4}>
+              <FormControl type="password" name="password" value={password} placeHolder="******" onChange={this.onChange}/>
+            </Col>
+        </FormGroup>
+
+        <FormGroup>
+          <Col smOffset={4} sm={4}>
+            <Button type="submit" bsStyle="primary" id="login-sign_btn">Login</Button>
+            <Button id="login-sign_btn">sign Up</Button>
+          </Col>
+        </FormGroup>
+      </Form>
+    </div>
+
+
       </div>
 
     );
   }
 }
+/** **/
+
 
 export default App;
