d1f767a5141bed54e9c5240afb8bb56901baea6a
Eoin Reilly <reilly_eoin@knights.ucf.edu>
2018-01-24 14:58:08 -0500

    Fixed file location

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,56 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
+
+import { AccessScreen } from './AccessScreen';
+
+
 
 class App extends Component {
-  state = {email:'', password:''};
 
-  /**componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }**/
-
-  onChange = (e) => {
-    const newState = this.state;
-    newState[e.target.name] = e.target.value;
-    this.setState(newState);
-  }
-
-  onSubmit = (e) => {
-    var post = {
-      "email":this.state.email,
-      "password":this.state.password
-    }
-    axios.post('http://localhost:3000/signup', post)
-      .then(function (result){
-        console.log(result);
-        if(result.data.code === 200){
-          console.log("HI");
-        }
-
-      })
-      .catch(function (error) {
-     console.log(error);
-   });
-  }
-
-  render() {
-    const { email, password } = this.state;
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        <form onSubmit={ this.onSubmit }>
-          <label>Email:</label>
-          <input type="email" name="email" value={email} onChange={this.onChange}/>
-          <label>Paswword:</label>
-          <input type="password" name="password" value={password} onChange={this.onChange}/>
-          <button type="submit">Login/sign Up</button>
-        </form>
-      </div>
-
+render(){
+      return(
+  <AccessScreen/>
     );
   }
 }
-
 export default App;
